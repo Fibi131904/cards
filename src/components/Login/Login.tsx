d911a68d96e385bc6e69531167f391a6bda41b19
Kseniya Smirnova <fanni@bk.ru>
2022-11-17 14:12:21 +0300

    fix: Login form customization redirect to registration form minimal styling

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,9 +1,22 @@
-import React from 'react';
-import { useFormik} from 'formik';
+import React, { useCallback } from 'react'
+import { useFormik } from 'formik'
 import s from './Login.module.css'
-import { loginTC } from '../../redux/auth-reducer';
-import { useTypedDispatch } from '../../redux/redux-store';
-
+import { loginTC } from '../../redux/auth-reducer'
+import { AppStateType, useTypedDispatch } from '../../redux/redux-store'
+import {
+  Button,
+  Checkbox,
+  FormControl,
+  FormControlLabel,
+  IconButton,
+  Input, 
+  InputAdornment, 
+  InputLabel,
+  TextField,
+} from '@material-ui/core'
+import { useSelector } from 'react-redux'
+import { Link, Navigate } from 'react-router-dom'
+import { Visibility, VisibilityOff } from '@material-ui/icons'
 
 
 type FormikErrorType = {
@@ -12,74 +25,119 @@ type FormikErrorType = {
   rememberMe?: false
 }
 
+type StatePassword = {
+  password: string;
+  showPassword: boolean;
+}
 
-   export const Login = () => {
-    const dispatch=useTypedDispatch()
-    const formik = useFormik({
-      initialValues: {
-        email: '',
-         password: '' ,
-         rememberMe: false
-        },
-        validate:(values) => {
-          const errors:FormikErrorType = {};
-          if (!values.email) {
-            errors.email = 'Required';
-          }
-          if (
-            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-          ) {
-            errors.email = 'Invalid email address';
-          }
-          if (!values.password) {
-            errors.password = 'Required';
-        } else if (values.password.length <= 7) {
-            errors.password = 'Password must be more than 7 characters...'
-        }
-        return errors;
-        },
-        onSubmit: values=>{
-          dispatch(loginTC(values))
-        } 
-    })
-    return (
-    <div className={s.loginBlock}>
-      
-      <div className={s.container}>
+export const Login = () => {
+  const isLoggedIn= useSelector<AppStateType, boolean>((state)=>state.auth.isLoggedIn)
+  const dispatch = useTypedDispatch()
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+      rememberMe: false,
+    },
+    validate: (values) => {
+      const errors: FormikErrorType = {}
+      if (!values.email) {
+        errors.email = 'Required'
+      }
+      if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+        errors.email = 'Invalid email address'
+      }
+      if (!values.password) {
+        errors.password = 'Required'
+      } else if (values.password.length <= 7) {
+        errors.password = 'Password must be more than 7 characters...'
+      }
+      return errors
+    },
+    onSubmit: (values) => {
+      dispatch(loginTC(values))
+    },
+  })
+  const [valuesPassword, setValuesPassword] = React.useState<StatePassword>({
+    password: '',
+    showPassword: false,
+});
+
+const handleClickShowPassword = useCallback(() => {
+    setValuesPassword({
+        ...valuesPassword,
+        showPassword: !valuesPassword.showPassword,
+    });
+}, [valuesPassword]);
+
+const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+};
+
+  if(isLoggedIn){
+    return <Navigate to={'/profile'}/>
+}
+  return (
+    <div className={s.container}>
       <h2>Login</h2>
-      <form onSubmit={formik.handleSubmit} className={s.formLogin}>
-      <label htmlFor="email">Email Address</label>
-      <input
-       onChange={formik.handleChange}
-       value={formik.values.email}
-       onBlur={formik.handleBlur}
-       id='email'
-       name="email"/>
-      {formik.touched.email && formik.errors.email?<div className={s.error}>{formik.errors.email}</div>: null}
-      <label htmlFor="password">Password</label>
-      <input 
-      type='password'
-       onChange={formik.handleChange}
-       value={formik.values.password}
-       onBlur={formik.handleBlur}
-       id='password'
-       name="password"/>
-      {formik.touched.password && formik.errors.password?<div className={s.error}>{formik.errors.password}</div>: null} 
 
-      <label htmlFor="checkbox" >Remember me</label>
-      <input 
-      type ='checkbox'
-      onChange={formik.handleChange}
-       checked={formik.values.rememberMe}
-       id='rememberMe'
-       name='rememberMe'/>
+      <form onSubmit={formik.handleSubmit} className={s.form}>
+        <FormControl>
 
-      <button type="submit">
-       Login
-      </button>
-    </form>
+       
+
+          <TextField
+            label="Email"
+            margin="normal"
+            {...formik.getFieldProps('email')}
+          />
+
+          {formik.touched.email && formik.errors.email ? (
+            <div className={s.error}>{formik.errors.email}</div>
+          ) : null}
+
+<InputLabel color="primary">Password</InputLabel>
+                    <Input
+                        id="password"
+                        type={valuesPassword.showPassword ? 'text' : 'password'}
+                        placeholder={'Password'}
+                        className={s.input}
+                        color="primary"
+                        {...formik.getFieldProps('password')}
+                        autoComplete="on"
+                        endAdornment={
+                            <InputAdornment position="end">
+                                <IconButton
+                                    onClick={handleClickShowPassword}
+                                    onMouseDown={handleMouseDownPassword}
+                                >
+                                    {valuesPassword.showPassword ? <VisibilityOff/> : <Visibility/>}
+                                </IconButton>
+                            </InputAdornment>
+                        }
+                    />
+          {formik.touched.password && formik.errors.password ? (
+            <div className={s.error}>{formik.errors.password}</div>
+          ) : null}
+
+          <FormControlLabel
+            label={'Remember me'}
+            checked={formik.values.rememberMe}
+            control={
+              <Checkbox
+                color="primary"
+                {...formik.getFieldProps('rememberMe')}
+              />
+            }
+          />
+
+          <Button type={'submit'} variant={'contained'} color={'primary'}>
+            Login
+          </Button>
+          </FormControl>
+          <div className={s.linkText}>Alredy have an account?</div>
+          <Link to="/register" >Regisration</Link> 
+      </form>
     </div>
-    </div>
-    )
-  }
-  
+  )
+}
